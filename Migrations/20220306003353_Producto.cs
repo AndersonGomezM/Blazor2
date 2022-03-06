668287ffcf33a10dc0e8beed146d46972e41dc50
Anderson <97201511+AndersonGomezM@users.noreply.github.com>
2022-03-05 20:38:31 -0400

    Blazor un detalle mas

---
+++ Migrations/20220306003353_Producto.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blazor.Migrations
+{
+    public partial class Producto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false),
+                    Presentacion = table.Column<string>(type: "TEXT", nullable: false),
+                    Existencia = table.Column<int>(type: "INTEGER", nullable: false),
+                    Costo = table.Column<double>(type: "REAL", nullable: false),
+                    Precio = table.Column<double>(type: "REAL", nullable: false),
+                    Ganancia = table.Column<int>(type: "INTEGER", nullable: false),
+                    ValorInventario = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.ProductoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductosDetalle",
+                columns: table => new
+                {
+                    DetallesId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProductosId = table.Column<int>(type: "INTEGER", nullable: false),
+                    DescripcionDetalle = table.Column<string>(type: "TEXT", nullable: true),
+                    Cantidad = table.Column<double>(type: "REAL", nullable: false),
+                    Precio = table.Column<double>(type: "REAL", nullable: false),
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductosDetalle", x => x.DetallesId);
+                    table.ForeignKey(
+                        name: "FK_ProductosDetalle_Productos_ProductoId",
+                        column: x => x.ProductoId,
+                        principalTable: "Productos",
+                        principalColumn: "ProductoId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductosDetalle_ProductoId",
+                table: "ProductosDetalle",
+                column: "ProductoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductosDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
